54c43238fd3c5951887612456261e0910fa4a3ac
Kinnari Bhalerao <kinbhalerao5@gmail.com>
2025-04-11 20:21:49 +0530

    New commit

---
+++ python_basics/calculator.py
@@ -0,0 +1,13 @@
+num1= float(input("Enter first num"))
+num2= float(input("Enter second num"))
+
+summation = num1+num2
+diff = num1 - num2 
+product = num1 * num2
+quotient = num1 / num2 
+
+print(summation)
+print(diff)
+print(product)
+print(quotient)
+
